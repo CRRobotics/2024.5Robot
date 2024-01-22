783fa914176bc84cd37877c6788ee174c0253421
Ethan-h48 <122328136+Ethan-h48@users.noreply.github.com>
2024-01-22 18:51:39 -0500

    Update Aquisition.java
    constructed aquisitionMotor

--- src/main/java/frc/robot/subsystems/Aquisition.java
+++ src/main/java/frc/robot/subsystems/Aquisition.java
@@ -1,3 +1,9 @@
+package frc.robot.subsystems;
+
+import com.robotics.CANSparkMax;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
 public class Aquisition extends SubsystemBase
 {
+  CANSparkMax aquisitionMotor = new CANSparkMax(0,CANSparkMaxLowLevel.MotorType.kBrushless);
 }
