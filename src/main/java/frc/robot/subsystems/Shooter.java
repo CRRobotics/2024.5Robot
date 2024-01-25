00fd544f26da8547e57f71f0983e27243ce01409
Ethan-h48 <122328136+Ethan-h48@users.noreply.github.com>
2024-01-24 20:50:35 -0500

    Improve Shooter and Winch

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -6,6 +6,21 @@ public class Shooter extends SubsystemBase
 {
     public Shooter()
     {
+        TalonFX leftShooterMotor = new TalonFX(0);
+        TalonFX rightShooterMotor = new TalonFX(0);
+        rightConfig = new TalonFXConfiguration();
+        leftConfig = new TalonFXConfiguration();
+        leftShooterMotor.config_kP(0,0);
+        leftShooterMotor.config_kI(0,0);
+        leftShooterMotor.config_kD(0,0);
+        rightShooterMotor.config_kP(0,0);
+        rightShooterMotor.config_kI(0,0);
+        rightShooterMotor.config_kD(0,0);
+    }
 
+    public void fire(double setPoint)
+    {
+        leftShooterMotor.set(TalonFXControlMode.Velocity, setPoint);
+        rightShooterMotor.set(TalonFXControlMode.Velocity, setPoint)
     }
 }
