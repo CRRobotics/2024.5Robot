37d85c3738f23b6f5aa3f0271edc53cad315bc1a
shoewhen <wshu07@gmail.com>
2024-01-25 19:26:11 -0500

    fixed imports + added comments

--- src/main/java/frc/robot/subsystems/Acquisition.java
+++ src/main/java/frc/robot/subsystems/Acquisition.java
@@ -1,27 +1,45 @@
 package frc.robot.subsystems;
 
-import com.robotics.CANSparkMax;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+/**
+ * Simulates the acquisition subsystem
+ */
 public class Acquisition extends SubsystemBase
 {
-  public acquisition()
+  CANSparkMax aqMotor;
+
+  /**
+   * Initializes the acquisition
+   */
+  public Acquisition()
   {
-    CANSparkMax aqMotor = new CANSparkMax(0,CANSparkMaxLowLevel.MotorType.kBrushless);
+    aqMotor = new CANSparkMax(0, MotorType.kBrushless);
   }
 
+  /**
+   * Intakes the cargo (not at a set speed yet)
+   */
   public void intake()
   {
-    aqMotor.set();
+    aqMotor.set(0);
   }
 
+  /**
+   * Stops the acquisition
+   */
   public void stop()
   {
     aqMotor.set(0);
   }
 
+  /**
+   * Ejects the cargo back out (not at a set speed yet)
+   */
   public void reject()
   {
-    aqMotor.set()
+    aqMotor.set(0);
   }
 }
