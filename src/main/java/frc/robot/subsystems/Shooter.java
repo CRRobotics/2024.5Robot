9237f94e3cb509be2e5b6a3063b80ba2aa55c609
shoewhen <wshu07@gmail.com>
2024-01-25 19:48:42 -0500

    fixed shooter code + added comments

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,17 +1,35 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import com.ctre.phoenix.motorcontrol.can.TalonFXConfiguration;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+/**
+ * Simulates the launcher subsystem
+ */
 public class Shooter extends SubsystemBase
 {
-    RealativeEncoder encoderL;
-    RealativeEncoder encoderR;
+    TalonFX leftShooterMotor;
+    TalonFX rightShooterMotor;
+    CANSparkMax leftPivotMotor;
+    CANSparkMax rightPivotMotor;
+    TalonFXConfiguration rightConfig;
+    TalonFXConfiguration leftConfig;
+    PIDController pid;
+    RelativeEncoder encoderL;
+    RelativeEncoder encoderR;
+
     public Shooter()
     {
-        TalonFX leftShooterMotor = new TalonFX(0);
-        TalonFX rightShooterMotor = new TalonFX(0);
-        CANSparkMax leftPivotMotor = new TalonFX(0);
-        CANSparkMax rightPivotMotor = new TalonFX(0);
+        leftShooterMotor = new TalonFX(0);
+        rightShooterMotor = new TalonFX(0);
+        leftPivotMotor = new CANSparkMax(0, MotorType.kBrushless);
+        rightPivotMotor = new CANSparkMax(0, MotorType.kBrushless);
         rightConfig = new TalonFXConfiguration();
         leftConfig = new TalonFXConfiguration();
         leftShooterMotor.config_kP(0,0);
@@ -20,20 +38,28 @@ public class Shooter extends SubsystemBase
         rightShooterMotor.config_kP(0,0);
         rightShooterMotor.config_kI(0,0);
         rightShooterMotor.config_kD(0,0);
-        PIDController pid = new PIDController(kP, kI, kD);
-        encoderL = LeftClimbMotor.encoder;
-        encoderR = RightClimbMotor.encoder;
+        pid = new PIDController(kP, kI, kD); // will edit later
+        encoderL = leftPivotMotor.getEncoder();
+        encoderR = rightPivotMotor.getEncoder();
     }
 
+    /**
+     * Fires the launcher at a set point
+     * @param setPoint The set point to fire the launcher at
+     */
     public void fire(double setPoint)
     {
         leftShooterMotor.set(TalonFXControlMode.Velocity, setPoint);
         rightShooterMotor.set(TalonFXControlMode.Velocity, setPoint);
     }
 
+    /**
+     * Aims the launcher to a set point
+     * @param setPoint The set point to aim the launcher at
+     */
     public void aim(double setPoint)
     {
-        leftPivotMotor.set(pid.calculate(encoderL.getDistance(), setPoint));
-        rightPivotMotor.set(pid.calculate(encoderR.getDistance(), setPoint));
+        leftPivotMotor.set(pid.calculate(encoderL.getPosition(), setPoint));
+        rightPivotMotor.set(pid.calculate(encoderR.getPosition(), setPoint));
     }
 }
