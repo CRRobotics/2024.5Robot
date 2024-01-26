c54e583fa401809433f0011ae9e0cabdf8d95612
Ethan-h48 <122328136+Ethan-h48@users.noreply.github.com>
2024-01-25 19:20:28 -0500

    Added pivot code into Shooter

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -4,10 +4,14 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Shooter extends SubsystemBase
 {
+    RealativeEncoder encoderL;
+    RealativeEncoder encoderR;
     public Shooter()
     {
         TalonFX leftShooterMotor = new TalonFX(0);
         TalonFX rightShooterMotor = new TalonFX(0);
+        CANSparkMax leftPivotMotor = new TalonFX(0);
+        CANSparkMax rightPivotMotor = new TalonFX(0);
         rightConfig = new TalonFXConfiguration();
         leftConfig = new TalonFXConfiguration();
         leftShooterMotor.config_kP(0,0);
@@ -16,11 +20,20 @@ public class Shooter extends SubsystemBase
         rightShooterMotor.config_kP(0,0);
         rightShooterMotor.config_kI(0,0);
         rightShooterMotor.config_kD(0,0);
+        PIDController pid = new PIDController(kP, kI, kD);
+        encoderL = LeftClimbMotor.encoder;
+        encoderR = RightClimbMotor.encoder;
     }
 
     public void fire(double setPoint)
     {
         leftShooterMotor.set(TalonFXControlMode.Velocity, setPoint);
-        rightShooterMotor.set(TalonFXControlMode.Velocity, setPoint)
+        rightShooterMotor.set(TalonFXControlMode.Velocity, setPoint);
+    }
+
+    public void aim(double setPoint)
+    {
+        leftPivotMotor.set(pid.calculate(encoderL.getDistance(), setPoint));
+        rightPivotMotor.set(pid.calculate(encoderR.getDistance(), setPoint));
     }
 }
