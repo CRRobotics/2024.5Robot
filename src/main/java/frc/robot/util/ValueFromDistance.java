a96381fe7f09cb765cfe29c387eae4a12becd2e2
Lucas West <LWest.0J@gmail.com>
2024-03-15 09:08:49 -0400

    Made getAngleSpeedLinearized immensesly case explicit

--- src/main/java/frc/robot/util/ValueFromDistance.java
+++ src/main/java/frc/robot/util/ValueFromDistance.java
@@ -15,7 +15,8 @@ public class ValueFromDistance {
         // shootMap.put(1 * 0.3048 + 1.23, new AngleSpeed(4.85, 160));
         // shootMap.put(2 * 0.3048 + 1.23, new AngleSpeed(4.95, 160));
         // shootMap.put(3 * 0.3048 + 1.23, new AngleSpeed(5.02, 160));
-        // shootMap.put(4 * 0.3048 + 1.23, new AngleSpeed(5.03, 160));
+        // shootMap.put(4 * 0.3048 + 1.23, new AngleSpeed(5.03, 160));\
+        shootMap.put(0.0, new AngleSpeed(4.79, 160));
         shootMap.put(1.29, new AngleSpeed(4.8, 160));
         shootMap.put(1.75, new AngleSpeed(4.9, 160));
         shootMap.put(2.13, new AngleSpeed(5, 160));
@@ -34,27 +35,33 @@ public class ValueFromDistance {
             return new AngleSpeed(angle, speed);
         }
         else return (distance < shootMap.firstKey()) ?
-                //THIS WORKS DONT THINK TOO HARD ABOUT IT
-                //gets the angle speed of the floor or ceiling key
-                new AngleSpeed(shootMap.get(shootMap.firstKey()).getAngle(), shootMap.get(shootMap.firstKey()).getSpeed()):
-                new AngleSpeed(shootMap.get(shootMap.lastKey()).getAngle(), shootMap.get(shootMap.lastKey()).getSpeed());
+            //THIS WORKS DONT THINK TOO HARD ABOUT IT
+            //gets the angle speed of the floor or ceiling key
+            new AngleSpeed(shootMap.get(shootMap.firstKey()).getAngle(), shootMap.get(shootMap.firstKey()).getSpeed()):
+            new AngleSpeed(shootMap.get(shootMap.lastKey()).getAngle(), shootMap.get(shootMap.lastKey()).getSpeed());
     }
 
     public static AngleSpeed getAngleSpeedLinearized(double distance) {
-            SmartDashboard.putNumber("distance from speaker", distance);
+        SmartDashboard.putNumber("distance from speaker", distance);
         if (distance > shootMap.firstKey() && distance < shootMap.lastKey()) {
-                double floorDistance = shootMap.floorKey(distance); 
-                double ceilingDistance = shootMap.ceilingKey(distance);
+            double floorDistance = shootMap.floorKey(distance); 
+            double ceilingDistance = shootMap.ceilingKey(distance);
 
-                double run = ceilingDistance - floorDistance;
-                
-                double speed = shootMap.get(floorDistance).getSpeed() + (distance - floorDistance) * ((shootMap.get(ceilingDistance).getSpeed() - shootMap.get(floorDistance).getSpeed())/run);
-                double angle = shootMap.get(floorDistance).getAngle() + (distance - floorDistance) * ((shootMap.get(ceilingDistance).getAngle() - shootMap.get(floorDistance).getAngle())/run);
+            double run = ceilingDistance - floorDistance;
+            
+            double speed = shootMap.get(floorDistance).getSpeed() + (distance - floorDistance) * ((shootMap.get(ceilingDistance).getSpeed() - shootMap.get(floorDistance).getSpeed())/run);
+            double angle = shootMap.get(floorDistance).getAngle() + (distance - floorDistance) * ((shootMap.get(ceilingDistance).getAngle() - shootMap.get(floorDistance).getAngle())/run);
 
-                return new AngleSpeed(angle, speed);
+            return new AngleSpeed(angle, speed);
         }
-        return distance <= shootMap.firstKey() ?
-            shootMap.get(shootMap.firstKey()) : shootMap.get(shootMap.lastKey());
+        else if (distance < shootMap.firstKey()) 
+        {
+            return shootMap.get(shootMap.firstKey());
+        }
+        else if (distance > shootMap.lastKey()) {
+            return shootMap.get(shootMap.lastKey());
+        }
+        else return shootMap.get(shootMap.firstKey());
     }
 
     public static AngleSpeed getAngleSpeedFloored(double distance) {
